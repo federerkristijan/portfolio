20bdd3ef4e1394b5f9a0131a83bff4aa1ea71c1a
Kristijan Federer <federer.kristijan@gmail.com>
2022-05-31 16:57:17 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home'
+import Home from './components/Home';
 import Layout from './components/Layout';
 // import Sidebar from './componenets/Sidebar';
 
