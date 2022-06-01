e850024640e90470ce2d22e5853db7b5a97e73f6
Kristijan Federer <federer.kristijan@gmail.com>
2022-06-01 16:58:16 +0200

    home/index rearranged

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,22 +1,25 @@
-import { Link } from 'react-router-dom';
-import Pic from '../../assets/images/kiki_pic.jpg';
-import './index.scss';
+import { Link } from 'react-router-dom'
+import Pic from '../../assets/images/kiki_pic.jpg'
+import './index.scss'
 
 const Home = () => {
-
   return (
-    <div className='container home-page'>
-      <div className='text-zone'>
-        <h1>Hi, <br /> I'm Kristijan
-        <img src={Pic} alt="profile_pic" />
-        <br />
-        Web developer
+    <div className="container home-page">
+      <img src={Pic} alt="profile_pic" />
+      <div className="text-zone">
+        <h1>
+          Hi, I'm Kristijan
         </h1>
-        <h2>Frontend developer</h2>
-        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+        <h2>
+          Frontend Web developer with full-stack background
+        </h2>
+        <h3>RoR, JS, React.js, Stimulus.js, PostgreSQL, etc.</h3>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
       </div>
     </div>
   )
 }
 
-export default Home;
+export default Home
