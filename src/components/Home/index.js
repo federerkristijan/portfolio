49f174ba90c27e78e508f8c461a8f00cdf0c9dd5
Kristijan Federer <federer.kristijan@gmail.com>
2022-06-01 18:44:35 +0200

    animatedLetters don't work locate the bug...happy hunting

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,78 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Pic from '../../assets/images/kiki_pic.jpg'
+import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['K', 'r', 'i', 's', 't', 'i', 'j', 'a', 'n']
+  const jobArray = [
+    'F',
+    'r',
+    'o',
+    'n',
+    't',
+    'e',
+    'n',
+    'd',
+    ' ',
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    ' ',
+    'w',
+    'i',
+    't',
+    'h',
+    ' ',
+    'a',
+    ' ',
+    'f',
+    'u',
+    'l',
+    'l',
+    's',
+    't',
+    'a',
+    'c',
+    'k',
+    ' ',
+    'b',
+    'a',
+    'c',
+    'k',
+    'g',
+    'r',
+    'o',
+    'u',
+    'n',
+    'd',
+  ]
+
   return (
     <div className="container home-page">
       <img src={Pic} alt="profile_pic" />
       <div className="text-zone">
         <h1>
-          Hi, I'm Kristijan
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`} >i,</span> <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m </span>
+          <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
         </h1>
         <h2>
-          Frontend Web developer with full-stack background
+          <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={15} />
         </h2>
         <h3>RoR, JS, React.js, Stimulus.js, PostgreSQL, etc.</h3>
         <Link to="/contact" className="flat-button">
