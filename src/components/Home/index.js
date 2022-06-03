820249c147e83afb652213737b09a7aa8671eaf0
Kristijan Federer <federer.kristijan@gmail.com>
2022-06-03 09:58:49 +0200

    logo/pic&text pimping finished

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,36 +2,38 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Pic from '../../assets/images/kiki_pic.jpg'
 import AnimatedLetters from '../AnimatedLetters'
-import Logo from './Logo'
+// hidding logo as I cannot manipulate it
+// import Logo from './Logo'
 import './index.scss'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
   const nameArray = [' ', 'K', 'r', 'i', 's', 't', 'i', 'j', 'a', 'n']
-  const jobArray = [
-    'F',
-    'r',
-    'o',
-    'n',
-    't',
-    'e',
-    'n',
-    'd',
-    ' ',
-    'w',
-    'e',
-    'b',
-    ' ',
-    'd',
-    'e',
-    'v',
-    'e',
-    'l',
-    'o',
-    'p',
-    'e',
-    'r',
-  ]
+  // removing jobArray, so that animation and the code looks nicer
+  // const jobArray = [
+  //   'F',
+  //   'r',
+  //   'o',
+  //   'n',
+  //   't',
+  //   'e',
+  //   'n',
+  //   'd',
+  //   ' ',
+  //   'w',
+  //   'e',
+  //   'b',
+  //   ' ',
+  //   'd',
+  //   'e',
+  //   'v',
+  //   'e',
+  //   'l',
+  //   'o',
+  //   'p',
+  //   'e',
+  //   'r',
+  // ]
 
   useEffect(() => {
       // removed return and then it works, research more...useEffect does and doesn't want to return
@@ -43,7 +45,7 @@ const Home = () => {
   return (
     <div className="container home-page">
       {/*temporary moved the pic until the logo position is solved */}
-      /*<img src={Pic} alt="profile_pic" />*/
+      <img src={Pic} alt="profile_pic" />
       <div className="text-zone">
         <h1>
           <span className={letterClass}>H</span>
@@ -53,15 +55,20 @@ const Home = () => {
           <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
         </h1>
         <h2>
-          <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={30} />
+          {/* <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={30} /> */}
+          <span className={`${letterClass} _16`}>Frontend</span>
+          <span className={`${letterClass} _17`}> </span>
+          <span className={`${letterClass} _18`}>Web</span>
+          <span className={`${letterClass} _19`}> </span>
+          <span className={`${letterClass} _20`}>Developer</span>
+          <span className={`${letterClass} _21`}> </span>
+          <span className={`${letterClass} _22`}>with</span>
           <span className={`${letterClass} _23`}> </span>
-          <span className={`${letterClass} _24`}>with</span>
+          <span className={`${letterClass} _24`}>a</span>
           <span className={`${letterClass} _25`}> </span>
-          <span className={`${letterClass} _26`}>a</span>
+          <span className={`${letterClass} _26`}>fullstack</span>
           <span className={`${letterClass} _27`}> </span>
-          <span className={`${letterClass} _28`}>fullstack</span>
-          <span className={`${letterClass} _29`}> </span>
-          <span className={`${letterClass} _30`}>background</span>
+          <span className={`${letterClass} _28`}>background</span>
 
         </h2>
         <h3>Ruby on Rails, Javascript, React.js, Stimulus.js, PostgreSQL, GitHub, Figma, etc.</h3>
@@ -69,7 +76,7 @@ const Home = () => {
           CONTACT ME
         </Link>
       </div>
-      <Logo />
+      {/* <Logo /> */}
     </div>
   )
 }
