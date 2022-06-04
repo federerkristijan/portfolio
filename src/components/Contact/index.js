ce6f46137735681db986fdec539f53598cc7ce30
Kristijan Federer <federer.kristijan@gmail.com>
2022-06-04 19:25:09 +0200

    emailjs doesn't work. going on the next topic

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,17 +1,40 @@
 import { useEffect, useState } from 'react'
+import { useRef } from 'react'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
+import emailjs from '@emailjs/browser'
 
 const Contact = (props) => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  const refForm = useRef
+  const form = useRef()
 
   useEffect(() => {
     setTimeout(() => {
       setLetterClass('text-animate-hover')
-    }, 4000)
+    }, 3000)
   }, [])
+
+  const sendEmail = e => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'gmail',
+        'template_92hpdem',
+        form.current,
+        '7wNWXf4jT4AUg4UJRbKyf'
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again!')
+        }
+      )
+  }
 
   return (
     <>
@@ -28,7 +51,7 @@ const Contact = (props) => {
             Some text
           </p>
           <div className="contact-form">
-            <form>
+            <form ref={form} on onSubmit={sendEmail}>
               <ul>
                 <li className='half'>
                   <input type="text" name='name' placeholder='name' required />
@@ -43,7 +66,7 @@ const Contact = (props) => {
                   <textarea placeholder='message' name='message' required />
                 </li>
                 <li>
-                  <input type='submit' className='flat-button' value='SEND' required />
+                  <input type='submit' className='flat-button' value='SEND' />
                 </li>
               </ul>
             </form>
