02c0d5af6795ed72303761e587f41101836a1afc
Kristijan Federer <federer.kristijan@gmail.com>
2022-06-04 17:18:35 +0200

    contact page almost done, letters animation needs to be fix on all pages

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,57 @@
+import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
-const Contact = () => {
-  return
+const Contact = (props) => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            Some text
+          </p>
+          <div className="contact-form">
+            <form>
+              <ul>
+                <li className='half'>
+                  <input type="text" name='name' placeholder='name' required />
+                </li>
+                <li className='half'>
+                  <input type="email" name='email' placeholder='email' required />
+                </li>
+                <li>
+                  <input placeholder='Subject' type='text' name='subject' required />
+                </li>
+                <li>
+                  <textarea placeholder='message' name='message' required />
+                </li>
+                <li>
+                  <input type='submit' className='flat-button' value='SEND' required />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
 }
 
 export default Contact
