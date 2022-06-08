8e4cd577fe3618cf439de0183917176e19108490
Kristijan Federer <federer.kristijan@gmail.com>
2022-06-08 11:20:58 +0200

    scoobie video added, needs adjustment

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,9 +1,16 @@
 import './index.scss'
+import Scoobie from '../../assets/images/scooby_app.mov'
 
 const Projects = props => {
 
   return (
-    <div></div>
+    <div className='projects-cont'>
+      <div>
+        <video controls='controls' width='500' height='300' className='scoobie'>
+          <source src={Scoobie} />
+        </video>
+      </div>
+    </div>
   )
 }
 
