44a21bdb3ca7dbf01829204bad3da46e33fc321e
Kristijan Federer <federer.kristijan@gmail.com>
2022-06-09 03:39:42 +0200

    sidebar done. home page to finish

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -46,12 +46,13 @@ const Home = () => {
   return (
     <>
     <div className="container home-page">
-      {/*temporary moved the pic until the logo position is solved */}
-      <img src={Pic} alt="profile_pic" />
+      <div>
+        <img src={Pic} alt="profile_pic" />
+      </div>
       <div className="text-zone">
         <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`} >i,</span> <br />
+          <span className={letterClass}>Hi</span>
+          <span className={`${letterClass} _12`} >,</span> <br />
           <span className={`${letterClass} _13`}>I</span>
           <span className={`${letterClass} _14`}>'m </span>
           <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
@@ -73,22 +74,28 @@ const Home = () => {
           <span className={`${letterClass} _28`}>background</span>
         </h2>
         <br />
-        <h3>
-          <span className={`${letterClass} _28`}>Ruby on Rails</span>
-          <span className={`${letterClass} _27`}>   </span>
-          <span className={`${letterClass} _28`}>Javascript</span>
-          <span className={`${letterClass} _27`}>   </span>
-          <span className={`${letterClass} _28`}>React.js</span>
-          <span className={`${letterClass} _27`}>   </span>
-          <span className={`${letterClass} _28`}>Stimulus.js</span>
-          <span className={`${letterClass} _27`}>   </span>
-          <span className={`${letterClass} _28`}>PostgreSQL</span>
-          <span className={`${letterClass} _27`}>   </span>
-          <span className={`${letterClass} _28`}>GitHub</span>
-          <span className={`${letterClass} _27`}>   </span>
-          <span className={`${letterClass} _28`}>Figma</span>
-          <span className={`${letterClass} _27`}>   </span>
-          <span className={`${letterClass} _28`}>Bootstrap</span>
+        <h3 className='text-box'>
+          <div className='white-text'>
+            <span className={`${letterClass}`}>Ruby on Rails</span>
+            <span className={`${letterClass} _27`}>   </span>
+            <span className={`${letterClass} _28`}>Javascript</span>
+            <span className={`${letterClass} _27`}>   </span>
+            <span className={`${letterClass} _28`}>React.js</span>
+            <span className={`${letterClass} _27`}>   </span>
+            <span className={`${letterClass} _28`}>Stimulus.js</span>
+            <span className={`${letterClass} _27`}>   </span>
+          </div>
+          <div className='red-text'>
+            <span className={`${letterClass} _28`}>PostgreSQL</span>
+            <span className={`${letterClass} _27`}>   </span>
+            <span className={`${letterClass} _28`}>GitHub</span>
+            <span className={`${letterClass} _27`}>   </span>
+            <span className={`${letterClass} _28`}>Figma</span>
+            <span className={`${letterClass} _27`}>   </span>
+            <span className={`${letterClass} _28`}>Bootstrap</span>
+            <span className={`${letterClass} _27`}>   </span>
+            <span className={`${letterClass} _28`}>SASS</span>
+          </div>
         </h3>
         <Link to="/contact" className="flat-button">
           CONTACT ME
