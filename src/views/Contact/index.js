0155d878add247c850e4a85d97985596f2970b2a
Kristijan Federer <federer.kristijan@gmail.com>
2022-06-14 21:33:47 +0200

    contact page aligned

--- src/views/Contact/index.js
+++ src/views/Contact/index.js
@@ -1,5 +1,4 @@
-import { useEffect, useState } from 'react'
-import { useRef } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../../components/AnimatedLetters'
 import './index.scss'
